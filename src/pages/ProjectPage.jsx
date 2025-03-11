b7b0d4a2f30dd3a45e641de0aaafbad113c9a0cc
subhi78 <kuwatsubhi78@gmail.com>
2025-03-11 13:00:08 +0700

    perbarui projectData

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -8,6 +8,14 @@ const ProjectPage = () => {
       response.json().then((data) => setDatas(data))
     );
   }, []);
+
+  const openProject = (url) => {
+    if (url) {
+      window.open(url, '_blank', 'noopener,noreferrer'); // Buka di tab baru
+    } else {
+      alert('URL tidak tersedia!');
+    }
+  };
   return (
     <>
       <h1 className="text-5xl font-bold">My Projects,</h1>
@@ -20,25 +28,18 @@ const ProjectPage = () => {
           <div
             className="card masonry-item bg-base-200 rounded-lg hover:scale-105 transition-all cursor-pointer"
             key={data.id}
+            onClick={() => openProject(data.url)}
           >
             <div className="card-body">
-              <figure className="aspect-video w-full">
+              <figure className="aspect-video w-full ">
                 <img
-                  src={'https://robohash.org/' + data.title}
+                  src={data.images}
                   alt={data.title}
+                  className="rounded-lg"
                 />
               </figure>
               <h3 className="card-title">{data.title}</h3>
               <p className="text-sm">{data.description}</p>
-              <div className="card-actions justify-end">
-                <a
-                  href={data.url}
-                  target="_blank"
-                  className="btn btn-primary rounded-lg"
-                >
-                  View
-                </a>
-              </div>
             </div>
           </div>
         ))}
